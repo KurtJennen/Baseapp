cf603cc67d6f6c4c216f924ec61aae266632fa06
Kurt Jennen <kurt.jennen@skynet.be>
2018-10-10 11:25:14 +0200

    Move menu queries from dao to mapping as named queries

--- src/main/java/be/luxuryoverdosis/framework/data/dao/implementations/MenuHibernateDAOMySQLImpl.java
+++ src/main/java/be/luxuryoverdosis/framework/data/dao/implementations/MenuHibernateDAOMySQLImpl.java
@@ -2,6 +2,7 @@ package be.luxuryoverdosis.framework.data.dao.implementations;
 
 import java.util.ArrayList;
 
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
 import be.luxuryoverdosis.framework.data.dao.AbstractHibernateDaoSupport;
@@ -12,16 +13,22 @@ import be.luxuryoverdosis.framework.logging.Logging;
 
 @Repository
 public class MenuHibernateDAOMySQLImpl extends AbstractHibernateDaoSupport implements MenuHibernateDAO {
+	private static final String DISABLED = "disabled";
+	private static final String FULL_NAME = "fullName";
+	private static final String HIDDEN = "hidden";
+	private static final String PAYED = "payed";
+	private static final String USER_ID = "userId";
+
 	public Menu createOrUpdate(final Menu menu) {
 		Logging.info(this, "Begin createMenu");
-		getHibernateTemplate().saveOrUpdate(menu);
+		getCurrentSession().saveOrUpdate(menu);
 		Logging.info(this, "End createMenu");
 		return menu;
 	}
 
 	public Menu read(final int id) {
 		Logging.info(this, "Begin readMenu");
-		Menu menu = (Menu) getHibernateTemplate().load(Menu.class, id);
+		Menu menu = (Menu) getCurrentSession().load(Menu.class, id);
 		Logging.info(this, "End readMenu");
 		return menu;
 	}
@@ -29,7 +36,12 @@ public class MenuHibernateDAOMySQLImpl extends AbstractHibernateDaoSupport imple
 	@SuppressWarnings("unchecked")
 	public Menu readFullName(final String fullName, final int userId) {
 		Logging.info(this, "Begin readFullName");
-		ArrayList<Menu> arrayList = (ArrayList<Menu>) getHibernateTemplate().find("from Menu m where m.fullName = ? and m.user.id = ?", new Object[]{fullName, userId});
+		
+		Query query = getCurrentSession().getNamedQuery("getMenusByFullNameAndUser");
+		query.setString(FULL_NAME, fullName);
+		query.setInteger(USER_ID, userId);
+		ArrayList<Menu> arrayList = (ArrayList<Menu>) query.list();
+		
 		Menu menu = null;
 		if(!arrayList.isEmpty()) {
 			menu = (Menu)arrayList.iterator().next();
@@ -40,80 +52,67 @@ public class MenuHibernateDAOMySQLImpl extends AbstractHibernateDaoSupport imple
 
 	public void delete(final int id) {
 		Logging.info(this, "Begin deleteMenu");
-		getHibernateTemplate().delete((Menu) getHibernateTemplate().load(Menu.class, id));
+		getCurrentSession().delete(this.read(id));
 		Logging.info(this, "End deleteMenu");		
 	}
 	
 	public void deleteForUser(int userId) {
 		Logging.info(this, "Begin deleteForUserMenu");
-		getHibernateTemplate().bulkUpdate("delete from Menu m where m.user.id = ?", new Object[]{userId});
+		
+		Query query = getCurrentSession().getNamedQuery("deleteMenusByUser");
+		query.setInteger(USER_ID, userId);
+		query.executeUpdate();
+		
 		Logging.info(this, "End deleteForUserMenu");	
 	}
 	
-	private static final String listSelectHql = "SELECT new be.luxuryoverdosis.framework.data.dto.MenuDTO(" +
-		"m.id, " +
-		"m.fullName, " +
-		"m.name, " +
-		"m.title, " +
-		"m.fullLevel, " +
-		"m.level, " +
-		"m.hidden, " +
-		"m.disabled, " +
-		"m.forPay, " +
-		"m.payed, " +
-		"u.id " +
-		") " +
-		"from Menu m " +
-		"inner join m.user u ";
-	
-	private static final String listHql = listSelectHql +
-		"where u.id = ? " +
-		"order by m.fullLevel";
-
 	@SuppressWarnings("unchecked")
 	public ArrayList<MenuDTO> list(final int userId) {
 		Logging.info(this, "Begin listMenu");
-		ArrayList<MenuDTO> arrayList = (ArrayList<MenuDTO>) getHibernateTemplate().find(listHql, new Object[]{userId});
+		
+		Query query = getCurrentSession().getNamedQuery("getMenusByUserDto");
+		query.setInteger(USER_ID, userId);
+		ArrayList<MenuDTO> arrayList = (ArrayList<MenuDTO>) query.list();
+		
 		Logging.info(this, "End listMenu");
 		return arrayList;
 	}
 	
-	private static final String listHiddenHql = listSelectHql +
-		"where u.id = ? " +
-		"and m.hidden = ? " +
-		"order by m.fullLevel desc";
-	
 	@SuppressWarnings("unchecked")
 	public ArrayList<MenuDTO> listHidden(final int userId, final String hidden) {
 		Logging.info(this, "Begin listHiddenMenu");
-		ArrayList<MenuDTO> arrayList = (ArrayList<MenuDTO>) getHibernateTemplate().find(listHiddenHql, new Object[]{userId, hidden});
+
+		Query query = getCurrentSession().getNamedQuery("getHiddenMenusDtoByUser");
+		query.setInteger(USER_ID, userId);
+		query.setString(HIDDEN, hidden);
+		ArrayList<MenuDTO> arrayList = (ArrayList<MenuDTO>) query.list();
+		
 		Logging.info(this, "End listHiddenMenu");
 		return arrayList;
 	}
 	
-	private static final String listDisabledHql = listSelectHql +
-		"where u.id = ? " +
-		"and m.disabled = ? " +
-		"order by m.fullLevel desc";
-	
 	@SuppressWarnings("unchecked")
 	public ArrayList<MenuDTO> listDisabled(final int userId, final String disabled) {
 		Logging.info(this, "Begin listDisabledMenu");
-		ArrayList<MenuDTO> arrayList = (ArrayList<MenuDTO>) getHibernateTemplate().find(listDisabledHql, new Object[]{userId, disabled});
+		
+		Query query = getCurrentSession().getNamedQuery("getDisabledMenusDtoByUser");
+		query.setInteger(USER_ID, userId);
+		query.setString(DISABLED, disabled);
+		ArrayList<MenuDTO> arrayList = (ArrayList<MenuDTO>) query.list();
+		
 		Logging.info(this, "End listDisabledMenu");
 		return arrayList;
 	}
 	
-	private static final String listForPayAndPayedHql = listSelectHql +
-		"where u.id = ? " +
-		"and m.forPay = 'J' " +
-		"and m.payed = ? " +
-		"order by m.fullLevel desc";
-	
 	@SuppressWarnings("unchecked")
 	public ArrayList<MenuDTO> listForPayAndPayed(final int userId, final String payed) {
 		Logging.info(this, "Begin listForPayAndPayedMenu");
-		ArrayList<MenuDTO> arrayList = (ArrayList<MenuDTO>) getHibernateTemplate().find(listForPayAndPayedHql, new Object[]{userId, payed});
+		
+		Query query = getCurrentSession().getNamedQuery("getForPayMenusDtoByUserAndPayed");
+		query.setInteger(USER_ID, userId);
+		query.setString(PAYED, payed);
+		ArrayList<MenuDTO> arrayList = (ArrayList<MenuDTO>) query.list();
+		
 		Logging.info(this, "End listForPayAndPayedMenu");
 		return arrayList;
 	}
@@ -121,8 +120,12 @@ public class MenuHibernateDAOMySQLImpl extends AbstractHibernateDaoSupport imple
 	@SuppressWarnings("unchecked")
 	public long count(final int userId) {
 		Logging.info(this, "Begin countMenu");
-		ArrayList<Long> arrayList = (ArrayList<Long>) getHibernateTemplate().find("select count(*) from Menu m where m.user.id = ?", new Object[]{userId});
+		
+		Query query = getCurrentSession().getNamedQuery("getCountMenusByUser");
+		query.setInteger(USER_ID, userId);
+		ArrayList<Long> arrayList = (ArrayList<Long>) query.list();
 		long count = arrayList.iterator().next().longValue();
+		
 		Logging.info(this, "End countMenu)");
 		return count;
 	}
@@ -130,8 +133,13 @@ public class MenuHibernateDAOMySQLImpl extends AbstractHibernateDaoSupport imple
 	@SuppressWarnings("unchecked")
 	public long count(final String fullName, final int userId) {
 		Logging.info(this, "Begin countMenu");
-		ArrayList<Long> arrayList = (ArrayList<Long>) getHibernateTemplate().find("select count(*) from Menu m where m.fullName = ? and m.user.id = ?", new Object[]{fullName, userId});
+		
+		Query query = getCurrentSession().getNamedQuery("getCountMenusByFullNameAndUser");
+		query.setString(FULL_NAME, fullName);
+		query.setInteger(USER_ID, userId);
+		ArrayList<Long> arrayList = (ArrayList<Long>) query.list();
 		long count = arrayList.iterator().next().longValue();
+		
 		Logging.info(this, "End countMenu)");
 		return count;
 	}
