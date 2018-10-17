fe40f64a4b14e48c347807516782e0f99fda4b88
Kurt Jennen <kurt.jennen@skynet.be>
2018-10-17 09:10:24 +0200

    Move batchJobInstance queries from dao to mapping as named queries

--- src/main/java/be/luxuryoverdosis/framework/data/dao/implementations/BatchJobInstanceHibernateDAOMySQLImpl.java
+++ src/main/java/be/luxuryoverdosis/framework/data/dao/implementations/BatchJobInstanceHibernateDAOMySQLImpl.java
@@ -2,6 +2,7 @@ package be.luxuryoverdosis.framework.data.dao.implementations;
 
 import java.util.ArrayList;
 
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
 import be.luxuryoverdosis.framework.data.dao.AbstractHibernateDaoSupport;
@@ -11,24 +12,24 @@ import be.luxuryoverdosis.framework.logging.Logging;
 
 @Repository
 public class BatchJobInstanceHibernateDAOMySQLImpl extends AbstractHibernateDaoSupport implements BatchJobInstanceHibernateDAO {
+	private static final String JOB_EXECUTION_ID = "jobExecutionId";
+	private static final String JOB_NAME = "jobName";
 	
 	public BatchJobInstance read(final long id) {
 		Logging.info(this, "Begin readBatchJobInstance");
-		BatchJobInstance batchJobInstance = (BatchJobInstance) getHibernateTemplate().load(BatchJobInstance.class, id);
+		BatchJobInstance batchJobInstance = (BatchJobInstance) getCurrentSession().load(BatchJobInstance.class, id);
 		Logging.info(this, "End readBatchJobInstance");
 		return batchJobInstance;
 	}
 	
-	private static final String readJobExecutionHql = "SELECT bji " +
-			"from BatchJobExecution bje " +
-			"inner join bje.batchJobInstance bji " +
-			"where bje.id = ? " +
-			"order by bje.createTime desc";
-	
 	@SuppressWarnings("unchecked")
 	public BatchJobInstance readJobExecution(final long jobExecutionId) {
 		Logging.info(this, "Begin readBatchJobInstance");
-		ArrayList<BatchJobInstance> arrayList = (ArrayList<BatchJobInstance>) getHibernateTemplate().find(readJobExecutionHql, new Object[]{jobExecutionId});
+		
+		Query query = getCurrentSession().getNamedQuery("getAllBatchJobInstanceByJobExecution");
+		query.setLong(JOB_EXECUTION_ID, jobExecutionId);
+		ArrayList<BatchJobInstance> arrayList = (ArrayList<BatchJobInstance>) query.list();
+		
 		BatchJobInstance batchJobInstance = null;
 		if(!arrayList.isEmpty()) {
 			batchJobInstance = (BatchJobInstance)arrayList.iterator().next();
@@ -37,29 +38,14 @@ public class BatchJobInstanceHibernateDAOMySQLImpl extends AbstractHibernateDaoS
 		return batchJobInstance;
 	}
 	
-	
-	private static final String listHql = "SELECT new be.luxuryoverdosis.framework.data.to.BatchJobInstance(" +
-		"bji.id, " +
-		"bji.jobName, " +
-		"bje.id, " +
-		"bje.version, " +
-		"bje.createTime, " +
-		"bje.startTime, " +
-		"bje.endTime, " +
-		"bje.status, " +
-		"bje.exitCode, " +
-		"bje.exitMessage, " +
-		"bje.lastUpdated " +
-		") " +
-		"from BatchJobExecution bje " +
-		"inner join bje.batchJobInstance bji " +
-		"where bji.jobName = ? " +
-		"order by bje.createTime desc";
-	
 	@SuppressWarnings("unchecked")
 	public ArrayList<BatchJobInstance> list(final String jobName) {
 		Logging.info(this, "Begin listBatchJobInstance");
-		ArrayList<BatchJobInstance> arrayList = (ArrayList<BatchJobInstance>) getHibernateTemplate().find(listHql, new Object[]{jobName});
+		
+		Query query = getCurrentSession().getNamedQuery("getAllBatchJobInstanceByJobName");
+		query.setString(JOB_NAME, jobName);
+		ArrayList<BatchJobInstance> arrayList = (ArrayList<BatchJobInstance>) query.list();
+		
 		Logging.info(this, "End listBatchJobInstance");
 		return arrayList;
 	}
