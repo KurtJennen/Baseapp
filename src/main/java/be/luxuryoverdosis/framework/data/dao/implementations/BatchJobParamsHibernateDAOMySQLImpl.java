3eab7f863e6292233655222dab74015397457c3f
Kurt Jennen <kurt.jennen@skynet.be>
2018-10-17 09:11:02 +0200

    Move batchJobParams queries from dao to mapping as named queries

--- src/main/java/be/luxuryoverdosis/framework/data/dao/implementations/BatchJobParamsHibernateDAOMySQLImpl.java
+++ src/main/java/be/luxuryoverdosis/framework/data/dao/implementations/BatchJobParamsHibernateDAOMySQLImpl.java
@@ -2,6 +2,7 @@ package be.luxuryoverdosis.framework.data.dao.implementations;
 
 import java.util.ArrayList;
 
+import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
 
 import be.luxuryoverdosis.framework.data.dao.AbstractHibernateDaoSupport;
@@ -11,10 +12,18 @@ import be.luxuryoverdosis.framework.logging.Logging;
 
 @Repository
 public class BatchJobParamsHibernateDAOMySQLImpl extends AbstractHibernateDaoSupport implements BatchJobParamsHibernateDAO {
+	private static final String JOB_INSTANCE_ID = "jobInstanceId";
+	private static final String KEY_NAME = "keyName";
+	
 	@SuppressWarnings("unchecked")
 	public BatchJobParams getJobParam(final long jobInstanceId, final String keyName) {
 		Logging.info(this, "Begin getJobParam");
-		ArrayList<BatchJobParams> arrayList = (ArrayList<BatchJobParams>) getHibernateTemplate().find("from BatchJobParams bjp where bjp.id = ? and bjp.keyName = ?", new Object[]{jobInstanceId, keyName});
+		
+		Query query = getCurrentSession().getNamedQuery("getAllJobParamsByJobInstanceAndKeyName");
+		query.setLong(JOB_INSTANCE_ID, jobInstanceId);
+		query.setString(KEY_NAME, keyName);
+		ArrayList<BatchJobParams> arrayList = (ArrayList<BatchJobParams>) query.list();
+		
 		BatchJobParams batchJobParams = null;
 		if(!arrayList.isEmpty()) {
 			batchJobParams = (BatchJobParams)arrayList.iterator().next();
@@ -26,7 +35,11 @@ public class BatchJobParamsHibernateDAOMySQLImpl extends AbstractHibernateDaoSup
 	@SuppressWarnings("unchecked")
 	public ArrayList<BatchJobParams> list(final long jobInstanceId) {
 		Logging.info(this, "Begin listBatchJobParams");
-		ArrayList<BatchJobParams> arrayList = (ArrayList<BatchJobParams>) getHibernateTemplate().find("from BatchJobParams bjp where bjp.id = ?", new Object[]{jobInstanceId});
+		
+		Query query = getCurrentSession().getNamedQuery("getAllJobParamsByJobInstance");
+		query.setLong(JOB_INSTANCE_ID, jobInstanceId);
+		ArrayList<BatchJobParams> arrayList = (ArrayList<BatchJobParams>) query.list();
+		
 		Logging.info(this, "End listBatchJobParams");
 		return arrayList;
 	}
