00673bf859c160711315f01fd7713fcdb6446eb3
Kurt Jennen <kurt.jennen@skynet.be>
2018-11-21 14:12:35 +0100

    Remove BatchJobExecution mapping and convert to JPA

--- src/main/java/be/luxuryoverdosis/framework/data/dao/implementations/BatchJobExecutionHibernateDAOMySQLImpl.java
+++ src/main/java/be/luxuryoverdosis/framework/data/dao/implementations/BatchJobExecutionHibernateDAOMySQLImpl.java
@@ -6,20 +6,19 @@ import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import be.luxuryoverdosis.framework.data.dao.AbstractHibernateDaoSupport;
+import be.luxuryoverdosis.framework.data.dao.QueryParameters;
 import be.luxuryoverdosis.framework.data.dao.interfaces.BatchJobExecutionHibernateDAO;
 import be.luxuryoverdosis.framework.data.to.BatchJobExecution;
 import be.luxuryoverdosis.framework.logging.Logging;
 
 @Repository
 public class BatchJobExecutionHibernateDAOMySQLImpl extends AbstractHibernateDaoSupport implements BatchJobExecutionHibernateDAO {
-	private static final String JOB_INSTANCE_ID = "jobInstanceId";
-	
 	@SuppressWarnings("unchecked")
 	public BatchJobExecution read(final long jobInstanceId) {
 		Logging.info(this, "Begin readBatchJobExecution");
 		
-		Query<BatchJobExecution> query = getCurrentSession().getNamedQuery("getAllBatchJobExecutionByJobInstance");
-		query.setParameter(JOB_INSTANCE_ID, jobInstanceId);
+		Query<BatchJobExecution> query = getCurrentSession().getNamedQuery(BatchJobExecution.SELECT_BATCH_JOB_EXECUTIONS_BY_JOB_INSTANCE);
+		query.setParameter(QueryParameters.JOB_INSTANCE_ID, jobInstanceId);
 		ArrayList<BatchJobExecution> arrayList = (ArrayList<BatchJobExecution>) query.list();
 		
 		BatchJobExecution batchJobExecution = null;
--- src/main/java/be/luxuryoverdosis/framework/data/to/BatchJobExecution.java
+++ src/main/java/be/luxuryoverdosis/framework/data/to/BatchJobExecution.java
@@ -2,16 +2,64 @@ package be.luxuryoverdosis.framework.data.to;
 
 import java.util.Date;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Proxy;
+
+@Entity
+@Table(name="batch_job_execution")
+@Access(AccessType.FIELD)
+@NamedQueries({
+	@NamedQuery(name=BatchJobExecution.SELECT_BATCH_JOB_EXECUTIONS_BY_JOB_INSTANCE, query=BatchJobExecution.Queries.SELECT_BATCH_JOB_EXECUTIONS_BY_JOB_INSTANCE)
+})
+@Proxy(lazy=false)
 public class BatchJobExecution {
+	public static final String SELECT_BATCH_JOB_EXECUTIONS_BY_JOB_INSTANCE = "selectBatchJobExecutionsByJobInstance";
+	
+	@Id
+	@Column(name="Job_Execution_Id")
 	private long id;
+	
+	@Column(name="Version")
 	private long version;
+	
+	@Column(name="Create_Time")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date createTime;
+	
+	@Column(name="Start_Time")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date startTime;
+	
+	@Column(name="End_Time")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date endTime;
+	
+	@Column(name="Status")
 	private String status;
+	
+	@Column(name="Exit_Code")
 	private String exitCode;
+	
+	@Column(name="Exit_Message")
 	private String exitMessage;
+	
+	@Column(name="Last_Updated")
 	private Date lastUpdated;
+	
+	@ManyToOne
+	@JoinColumn(name="Job_Instance_Id")
 	private BatchJobInstance batchJobInstance;
 	
 	public long getId() {
@@ -75,4 +123,11 @@ public class BatchJobExecution {
 		this.batchJobInstance = batchJobInstance;
 	}
 	
+	public static final class Queries {
+		public static final String SELECT_BATCH_JOB_EXECUTIONS_BY_JOB_INSTANCE = "select bje "
+				+ "from BatchJobExecution bje "
+				+ "inner join bje.batchJobInstance bji "
+				+ "where bji.id = :jobInstanceId "
+				+ "order by bje.createTime desc";
+	}
 }
