8041814d94b3e108d1b5c19189ab53595f19e4b8
Kurt Jennen <kurt.jennen@skynet.be>
2020-07-14 10:45:07 +0200

    Enable UTF8 filter for jsp to form data

---
+++ src/main/java/be/luxuryoverdosis/framework/web/security/CharacterEncodingFilter.java
@@ -0,0 +1,24 @@
+package be.luxuryoverdosis.framework.web.security;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class CharacterEncodingFilter implements Filter {
+	
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		filterChain.doFilter(request, response);
+	}
+	
+	public void destroy() {
+	}
+}
