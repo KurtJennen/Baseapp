de3b82a737f62838e5abdb14a0b4ad68abea185f
Kurt Jennen <kurt.jennen@skynet.be>
2023-01-27 13:45:19 +0100

    Angular frontend

--- src/main/java/be/luxuryoverdosis/framework/business/webservice/interfaces/UserRestService.java
+++ src/main/java/be/luxuryoverdosis/framework/business/webservice/interfaces/UserRestService.java
@@ -3,7 +3,7 @@ package be.luxuryoverdosis.framework.business.webservice.interfaces;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface UserRestService {
-	public String readUserRequest(String name) throws JsonProcessingException;
+	public String readUserRequest(String name, String password) throws JsonProcessingException;
 	public String readAllUsersRequest() throws JsonProcessingException;
 	public String createOrUpdateUserRequest(String name, String userName, String encryptedPassword, String email, String[] roleNames) throws JsonProcessingException;
 	public String deleteUserRequest(String name) throws JsonProcessingException;
