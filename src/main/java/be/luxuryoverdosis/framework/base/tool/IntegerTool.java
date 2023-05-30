300f093dcee9a3aade7a34c58dfcba6e39583d6a
Kurt Jennen <kurt.jennen@skynet.be>
2023-05-30 11:15:53 +0200

    Add convert string to int

--- src/main/java/be/luxuryoverdosis/framework/base/tool/IntegerTool.java
+++ src/main/java/be/luxuryoverdosis/framework/base/tool/IntegerTool.java
@@ -1,5 +1,18 @@
 package be.luxuryoverdosis.framework.base.tool;
 
+import org.apache.commons.lang.StringUtils;
+
 public class IntegerTool {
 	public static final String INTEGER_PATTERN = "#";
+	
+	public static int toInteger(String string) {
+		int convertedInteger = 0;
+		
+		if(string != null && StringUtils.isNotEmpty(string)) {
+			convertedInteger = Integer.parseInt(string);
+		}
+		
+		return convertedInteger;
+	}
+
 }
